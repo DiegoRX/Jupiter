e4424c9a9e1e39832b105f2c39d85580cbca2f4f
DiegoRX <dio351a@gmail.com>
2021-10-24 15:30:12 -0500

    header structure

--- src/container/Header/index.jsx
+++ src/container/Header/index.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
 const Header = () => {
-  return (<h1>Header</h1>);
+  return (
+    <div className="section-header">
+      <header className="Header">
+        <div className="Header_Wrapper">
+          <div className="Header_FlexItem">
+            <nav>
+              <ul className="HorizontalList">
+                <li className="HorizontalList__Item">Shop</li>
+                <li className="HorizontalList__Item">SHOP!"</li>
+              </ul>
+            </nav>
+          </div>
+          <div className="Header_FlexItem">
+            <p>LOGO</p>
+          </div>
+          <div className="Header_FlexItem">
+            <nav className="Header__SecondaryNav">
+              <ul className="HorizontalList">
+                <li className="HorizontalList__Item">Item1</li>
+                <li className="HorizontalList__Item">Item2</li>
+                <li className="HorizontalList__Item">Item2</li>
+                <li className="HorizontalList__Item">Item2</li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+    </div>
+
+
+  );
 };
 
 export default Header;
